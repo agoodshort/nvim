a490b5c150babf25b7d413ca3a929d1f3761c0f4
agoodshort <adrien.goodshort@gmail.com>
2023-11-28 16:05:29 +0000

    Matches heirline theme

--- lua/agoodshort/plugins/theme/incline.lua
+++ lua/agoodshort/plugins/theme/incline.lua
@@ -2,6 +2,9 @@ return {
 	"b0o/incline.nvim",
 	event = "VeryLazy",
 	config = function()
+		local kana_colors = require("kanagawa.colors").setup()
+		local theme = kana_colors.theme
+
 		local function get_diagnostic_label(props)
 			local icons = {
 				Error = "",
@@ -20,6 +23,18 @@ return {
 			return label
 		end
 
+		local function get_git_diff(props)
+			local icons = { removed = "", changed = "", added = "" }
+			local labels = {}
+			local signs = vim.api.nvim_buf_get_var(props.buf, "gitsigns_status_dict")
+			for name, icon in pairs(icons) do
+				if tonumber(signs[name]) and signs[name] > 0 then
+					table.insert(labels, { icon .. " " .. signs[name] .. " ", group = "Diff" .. name })
+				end
+			end
+			return labels
+		end
+
 		require("incline").setup({
 			window = {
 				placement = {
@@ -30,32 +45,43 @@ return {
 					horizontal = 2,
 					vertical = 2,
 				},
+				padding = 0,
 			},
 			render = function(props)
-				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":~:.")
-
-				local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
 				local diagnostics = get_diagnostic_label(props)
+				local git_diffs = get_git_diff(props)
+
+				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":~:.")
+				local filetype_icon, filetype_color = require("nvim-web-devicons").get_icon_color(filename)
+
+				local bg_color, fg_color, editor_bg_color
+				if props.focused == true then
+					bg_color = "black"
+					fg_color = theme.ui.fg
+                    editor_bg_color = theme.ui.bg
+				else
+					bg_color = theme.ui.bg
+					fg_color = theme.ui.nontext
+                    editor_bg_color = theme.ui.bg_dim
+				end
 
 				local buffer = {
 					{
 						filetype_icon,
-						guifg = color,
+						guifg = filetype_color,
 					},
 					{ " " },
 					{
 						filename,
+						guifg = fg_color,
 					},
 				}
-
-				if #diagnostics > 0 then
-					table.insert(diagnostics, { "| ", guifg = "grey" })
-				end
 
 				if vim.api.nvim_buf_get_option(props.buf, "modified") then
 					table.insert(buffer, { " [+]", guifg = "green", gui = "bold" })
 				end
 
+				-- Check if buffer is modifiable
 				if
 					not vim.api.nvim_buf_get_option(props.buf, "modifiable")
 					or vim.api.nvim_buf_get_option(props.buf, "readonly")
@@ -63,11 +89,29 @@ return {
 					table.insert(buffer, { " ", guifg = "orange", gui = "bold" })
 				end
 
+				-- Add separators
+				if #diagnostics > 0 then
+					table.insert(diagnostics, { "| ", guifg = "grey" })
+				end
+
+				if #git_diffs > 0 then
+					table.insert(git_diffs, { "| ", guifg = "grey" })
+				end
+
+				-- Add diagnostics and git diffs
 				for _, buffer_ in ipairs(buffer) do
 					table.insert(diagnostics, buffer_)
 				end
 
-				return diagnostics
+				for _, diagnostics_ in ipairs(diagnostics) do
+					table.insert(git_diffs, diagnostics_)
+				end
+
+				return {
+					{ "", guifg = bg_color, guibg = editor_bg_color },
+					{ git_diffs, guibg = bg_color },
+					{ "", guifg = bg_color, guibg = editor_bg_color },
+				}
 			end,
 		})
 	end,
