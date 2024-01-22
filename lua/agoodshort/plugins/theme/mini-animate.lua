c33ce96e14519391e6189e3a4f2acedf38890f15
agoodshort <adrien.goodshort@gmail.com>
2024-01-22 23:20:34 +0000

    fix(theme): fix mousescroll issue

--- lua/agoodshort/plugins/theme/mini-animate.lua
+++ lua/agoodshort/plugins/theme/mini-animate.lua
@@ -2,71 +2,74 @@ return {
 	"echasnovski/mini.animate",
 	version = "*",
 	event = "VeryLazy",
-	opts = {
-		-- Cursor path
-		cursor = {
-			-- Whether to enable this animation
-			enable = true,
+	config = function()
+		vim.opt.mousescroll = "ver:60,hor:6"
+		require("mini.animate").setup({
+			-- Cursor path
+			cursor = {
+				-- Whether to enable this animation
+				enable = true,
 
-			-- Timing of animation (how steps will progress in time)
-			-- timing = --<function: implements linear total 250ms animation duration>,
+				-- Timing of animation (how steps will progress in time)
+				-- timing = --<function: implements linear total 250ms animation duration>,
 
-			-- Path generator for visualized cursor movement
-			-- path = --<function: implements shortest line path>,
-		},
+				-- Path generator for visualized cursor movement
+				-- path = --<function: implements shortest line path>,
+			},
 
-		-- Vertical scroll
-		scroll = {
-			-- Whether to enable this animation
-			enable = true,
+			-- Vertical scroll
+			scroll = {
+				-- Whether to enable this animation
+				enable = true,
 
-			-- Timing of animation (how steps will progress in time)
-			-- timing = --<function: implements linear total 250ms animation duration>,
+				-- Timing of animation (how steps will progress in time)
+				-- timing = --<function: implements linear total 250ms animation duration>,
 
-			-- Subscroll generator based on total scroll
-			-- subscroll = --<function: implements equal scroll with at most 60 steps>,
-		},
+				-- Subscroll generator based on total scroll
+				-- subscroll = --<function: implements equal scroll with at most 60 steps>,
+			},
 
-		-- Window resize
-		resize = {
-			-- Whether to enable this animation
-			enable = true,
+			-- Window resize
+			resize = {
+				-- Whether to enable this animation
+				enable = true,
 
-			-- Timing of animation (how steps will progress in time)
-			-- timing = --<function: implements linear total 250ms animation duration>,
+				-- Timing of animation (how steps will progress in time)
+				-- timing = --<function: implements linear total 250ms animation duration>,
 
-			-- Subresize generator for all steps of resize animations
-			-- subresize = --<function: implements equal linear steps>,
-		},
+				-- Subresize generator for all steps of resize animations
+				-- subresize = --<function: implements equal linear steps>,
+			},
 
-		-- Window open
-		open = {
-			-- Whether to enable this animation
-			enable = true,
+			-- Window open
+			open = {
+				-- Whether to enable this animation
+				enable = true,
 
-			-- Timing of animation (how steps will progress in time)
-			-- timing = --<function: implements linear total 250ms animation duration>,
+				-- Timing of animation (how steps will progress in time)
+				-- timing = --<function: implements linear total 250ms animation duration>,
 
-			-- Floating window config generator visualizing specific window
-			-- winconfig = --<function: implements static window for 25 steps>,
+				-- Floating window config generator visualizing specific window
+				-- winconfig = --<function: implements static window for 25 steps>,
 
-			-- 'winblend' (window transparency) generator for floating window
-			-- winblend = --<function: implements equal linear steps from 80 to 100>,
-		},
+				-- 'winblend' (window transparency) generator for floating window
+				-- winblend = --<function: implements equal linear steps from 80 to 100>,
+			},
 
-		-- Window close
-		close = {
-			-- Whether to enable this animation
-			enable = true,
+			-- Window close
+			close = {
+				-- Whether to enable this animation
+				enable = true,
 
-			-- Timing of animation (how steps will progress in time)
-			-- timing = --<function: implements linear total 250ms animation duration>,
+				-- Timing of animation (how steps will progress in time)
+				-- timing = --<function: implements linear total 250ms animation duration>,
 
-			-- Floating window config generator visualizing specific window
-			-- winconfig = --<function: implements static window for 25 steps>,
+				-- Floating window config generator visualizing specific window
+				-- winconfig = --<function: implements static window for 25 steps>,
 
-			-- 'winblend' (window transparency) generator for floating window
-			-- winblend = --<function: implements equal linear steps from 80 to 100>,
-		},
-	},
+				-- 'winblend' (window transparency) generator for floating window
+				-- winblend = --<function: implements equal linear steps from 80 to 100>,
+			},
+		})
+	end,
 }
