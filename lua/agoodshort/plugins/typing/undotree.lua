8da0c4cee0cfb6c806a5b4d8c89258a904f363e2
agoodshort <adrien.goodshort@gmail.com>
2024-02-10 12:42:43 +0000

    feat(undotree): change layout and auto-focus

--- lua/agoodshort/plugins/typing/undotree.lua
+++ lua/agoodshort/plugins/typing/undotree.lua
@@ -2,6 +2,7 @@ return {
 	"mbbill/undotree",
 	config = function()
 		vim.opt.undofile = true
-		vim.g.undotree_WindowLayout = 3
+		vim.g.undotree_WindowLayout = 4
+		vim.g.undotree_SetFocusWhenToggle = 1
 	end,
 }
