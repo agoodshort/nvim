78509d3710c57d741a2dd234f7009a8e0b9a36b4
agoodshort <adrien.goodshort@gmail.com>
2024-06-21 00:54:25 +0100

    feat(node): add `import-cost.nvim`

--- lua/agoodshort/plugins/language-specific/javascript.lua
+++ lua/agoodshort/plugins/language-specific/javascript.lua
@@ -1,6 +1,13 @@
 return {
-	"vuki656/package-info.nvim",
-	event = { "BufRead package.json" },
-	dependencies = "MunifTanjim/nui.nvim",
-	opts = {},
+	{
+		"vuki656/package-info.nvim",
+		event = { "BufRead package.json" },
+		dependencies = "MunifTanjim/nui.nvim",
+		opts = {},
+	},
+	{
+		"barrett-ruth/import-cost.nvim",
+		build = "sh install.sh npm",
+		opts = {},
+	},
 }
