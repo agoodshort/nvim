1ce8ce71c971c0efa2b9d551c811d85e8daea0ff
agoodshort <adrien.goodshort@gmail.com>
2024-07-27 02:30:22 +0100

    fix(treesitter): could not close html tags

--- lua/agoodshort/plugins/theme/nvim-treesitter.lua
+++ lua/agoodshort/plugins/theme/nvim-treesitter.lua
@@ -1,55 +1,47 @@
 return {
-	{
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "BufReadPost",
+	dependencies = {
+		{ "windwp/nvim-ts-autotag", config = true },
 		"nvim-treesitter/nvim-treesitter-context",
-		event = "VeryLazy",
-		dependencies = "nvim-treesitter/nvim-treesitter",
 	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		event = "BufReadPost",
-		dependencies = "windwp/nvim-ts-autotag",
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"regex",
+				"hyprlang",
+				"git_config",
+			},
+			highlight = { enable = true },
+			autotag = { enable = false },
+			indent = { enable = true },
+			-- select blocks with <CR>
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<CR>",
+					node_incremental = "<CR>",
+					scope_incremental = "<S-CR>",
+					node_decremental = "<BS>",
 				},
-				autotag = {
-					enable = true,
-				},
-				indent = { enable = true },
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"markdown",
-					"markdown_inline",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"regex",
-					"hyprlang",
-					"git_config",
-				},
-				-- select blocks with <CR>
-				auto_install = true,
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<CR>",
-						node_incremental = "<CR>",
-						scope_incremental = "<S-CR>",
-						node_decremental = "<BS>",
-					},
-				},
-			})
-		end,
-	},
+			},
+		})
+	end,
 }
