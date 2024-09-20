ccea84aa48ad33c7dab203a89dabe07e577534c2
agoodshort <adrien.goodshort@gmail.com>
2024-09-20 22:03:03 +0100

    feat(dap): lazy loading and update layout

--- lua/agoodshort/plugins/language-specific/nvim-dap.lua
+++ lua/agoodshort/plugins/language-specific/nvim-dap.lua
@@ -4,16 +4,103 @@
 return {
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		opts = {},
+		lazy = true,
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			controls = {
+				element = "repl",
+				enabled = true,
+				icons = {
+					disconnect = "",
+					pause = "",
+					play = "",
+					run_last = "",
+					step_back = "",
+					step_into = "",
+					step_out = "",
+					step_over = "",
+					terminate = "",
+				},
+			},
+			element_mappings = {},
+			expand_lines = true,
+			floating = {
+				border = "single",
+				mappings = {
+					close = { "q", "<Esc>" },
+				},
+			},
+			force_buffers = true,
+			icons = {
+				collapsed = "",
+				current_frame = "",
+				expanded = "",
+			},
+			layouts = {
+				{
+					elements = {
+						-- {
+						-- 	id = "scopes",
+						-- 	size = 0.25,
+						-- },
+						{
+							id = "breakpoints",
+							size = 0.2,
+						},
+						-- {
+						-- 	id = "stacks",
+						-- 	size = 0.2,
+						-- },
+						{
+							id = "watches",
+							size = 0.8,
+						},
+					},
+					position = "left",
+					size = 40,
+				},
+				{
+					elements = {
+						{
+							id = "repl",
+							size = 1,
+						},
+						-- {
+						-- 	id = "console",
+						-- 	size = 1,
+						-- },
+					},
+					position = "bottom",
+					size = 10,
+				},
+			},
+			mappings = {
+				edit = "e",
+				expand = { "<CR>", "<2-LeftMouse>" },
+				open = "o",
+				remove = "d",
+				repl = "r",
+				toggle = "t",
+			},
+			render = {
+				indent = 1,
+				max_value_lines = 100,
+			},
+		},
 	},
 	{
 		"theHamsta/nvim-dap-virtual-text",
+		lazy = true,
 		dependencies = "mfussenegger/nvim-dap",
 		opts = {},
 	},
 	{
 		"mfussenegger/nvim-dap",
+		lazy = true,
 		config = function()
 			require("dap").adapters["pwa-node"] = {
 				type = "server",
