25c2897cf1371edce9483a0e3fa6f51bcdfaee83
Adrien Biencourt <adrien.goodshort@gmail.com>
2024-11-06 14:47:09 +0000

    refactor: `nvim-dap` config cleanup

--- lua/agoodshort/plugins/language-specific/nvim-dap.lua
+++ lua/agoodshort/plugins/language-specific/nvim-dap.lua
@@ -3,100 +3,205 @@
 -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#javascript
 return {
 	{
-		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
 		lazy = true,
+		dependencies = "mfussenegger/nvim-dap",
+		opts = {},
+	},
+	{
+		"rcarriga/nvim-dap-ui",
 		dependencies = {
 			"mfussenegger/nvim-dap",
 			"nvim-neotest/nvim-nio",
 			"nvim-telescope/telescope.nvim",
 		},
-		opts = {
-			controls = {
-				element = "repl",
-				enabled = true,
+		config = function()
+			require("dapui").setup({
+				controls = {
+					element = "repl",
+					enabled = true,
+					icons = {
+						disconnect = "",
+						pause = "",
+						play = "",
+						run_last = "",
+						step_back = "",
+						step_into = "",
+						step_out = "",
+						step_over = "",
+						terminate = "",
+					},
+				},
+				element_mappings = {},
+				expand_lines = true,
+				floating = {
+					border = "single",
+					mappings = {
+						close = { "q", "<Esc>" },
+					},
+				},
+				force_buffers = true,
 				icons = {
-					disconnect = "",
-					pause = "",
-					play = "",
-					run_last = "",
-					step_back = "",
-					step_into = "",
-					step_out = "",
-					step_over = "",
-					terminate = "",
+					collapsed = "",
+					current_frame = "",
+					expanded = "",
 				},
-			},
-			element_mappings = {},
-			expand_lines = true,
-			floating = {
-				border = "single",
+				layouts = {
+					{
+						elements = {
+							-- {
+							-- 	id = "scopes",
+							-- 	size = 0.25,
+							-- },
+							{
+								id = "breakpoints",
+								size = 0.2,
+							},
+							-- {
+							-- 	id = "stacks",
+							-- 	size = 0.2,
+							-- },
+							{
+								id = "watches",
+								size = 0.8,
+							},
+						},
+						position = "left",
+						size = 40,
+					},
+					{
+						elements = {
+							{
+								id = "repl",
+								size = 1,
+							},
+							-- {
+							-- 	id = "console",
+							-- 	size = 1,
+							-- },
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
 				mappings = {
-					close = { "q", "<Esc>" },
+					edit = "e",
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					repl = "r",
+					toggle = "t",
 				},
-			},
-			force_buffers = true,
-			icons = {
-				collapsed = "",
-				current_frame = "",
-				expanded = "",
-			},
-			layouts = {
+				render = {
+					indent = 1,
+					max_value_lines = 100,
+				},
+			})
+
+			require("which-key").add({
+				{ "<leader>d", group = "Debugger" },
+				{ "<leader>df", group = "float" },
 				{
-					elements = {
-						-- {
-						-- 	id = "scopes",
-						-- 	size = 0.25,
-						-- },
-						{
-							id = "breakpoints",
-							size = 0.2,
-						},
-						-- {
-						-- 	id = "stacks",
-						-- 	size = 0.2,
-						-- },
-						{
-							id = "watches",
-							size = 0.8,
-						},
-					},
-					position = "left",
-					size = 40,
+					"<leader>dfr",
+					function()
+						require("dapui").float_element("repl", { enter = true, height = 50, width = 100 })
+					end,
+					desc = "Dap-ui Repl",
 				},
 				{
-					elements = {
-						{
-							id = "repl",
-							size = 1,
-						},
-						-- {
-						-- 	id = "console",
-						-- 	size = 1,
-						-- },
-					},
-					position = "bottom",
-					size = 10,
+					"<leader>dfw",
+					function()
+						require("dapui").float_element("watches", { enter = true, height = 50, width = 100 })
+					end,
+					desc = "Dap-ui Watches",
 				},
-			},
-			mappings = {
-				edit = "e",
-				expand = { "<CR>", "<2-LeftMouse>" },
-				open = "o",
-				remove = "d",
-				repl = "r",
-				toggle = "t",
-			},
-			render = {
-				indent = 1,
-				max_value_lines = 100,
-			},
-		},
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		lazy = true,
-		dependencies = "mfussenegger/nvim-dap",
-		opts = {},
+				{
+					"<leader>dt",
+					function()
+						require("dapui").toggle()
+					end,
+					desc = "Dap-ui Toggle",
+				},
+				{
+					"<leader>dR",
+					function()
+						require("dapui").open({ reset = true })
+					end,
+					desc = "Dap-ui Reset",
+				},
+				{
+					"<leader>db",
+					function()
+						require("dap").toggle_breakpoint()
+					end,
+					desc = "Toggle Dap Breakpoint",
+				},
+				{
+					"<leader>dC",
+					function()
+						require("dap").run_to_cursor()
+					end,
+					desc = "Run Dap to Cursor",
+				},
+				{
+					"<leader>dc",
+					function()
+						require("dap").continue()
+					end,
+					desc = "Dap Continue",
+				},
+				{
+					"<leader>dr",
+					function()
+						require("dap").restart()
+					end,
+					desc = "Dap Restart",
+				},
+				{
+					"<leader>dl",
+					function()
+						require("dap").run_last()
+					end,
+					desc = "Dap Run Last",
+				},
+				{
+					"<leader>dx",
+					function()
+						require("dap").terminate()
+					end,
+					desc = "Dap Terminate",
+				},
+				{
+					"<leader>do",
+					function()
+						require("dap").step_over()
+					end,
+					desc = "Dap Step-Over",
+				},
+				{
+					"<leader>di",
+					function()
+						require("dap").step_into()
+					end,
+					desc = "Dap Step-Into",
+				},
+				{
+					"<leader>de",
+					function()
+						require("dapui").eval(nil, { enter = true })
+					end,
+					desc = "Dap-ui Eval",
+				},
+				{
+					"<leader>da",
+					function()
+						require("dapui").elements.watches.add()
+					end,
+					desc = "Dap-ui Add to Watches",
+					mode = { "n", "v" },
+				},
+			})
+		end,
 	},
 	{
 		"mfussenegger/nvim-dap",
